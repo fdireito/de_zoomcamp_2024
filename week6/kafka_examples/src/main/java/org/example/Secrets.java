2986a5d3a455d4523fa3246d0694bd46bc11d8fa
fdireito <fredericopreto@gmail.com>
2024-03-05 19:51:31 +0000

    week 6 start

---
+++ week6/kafka_examples/src/main/java/org/example/Secrets.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Secrets {
+    public static final String KAFKA_CLUSTER_KEY = "NA6WQZJN2N72NPHB";
+    public static final String KAFKA_CLUSTER_SECRET = "BhisBkqxtdE1jQkda2RR0VMA07nWRriydmSzPxiU/wxy+46MMNIEMkrryjWGDODO";
+
+    public static final String SCHEMA_REGISTRY_KEY = "REPLACE_WITH_SCHEMA_REGISTRY_KEY";
+    public static final String SCHEMA_REGISTRY_SECRET = "REPLACE_WITH_SCHEMA_REGISTRY_SECRET";
+
+}
